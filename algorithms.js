4bb8cd944247b9cb5a391c0a8df55c1c23997a13
Ethan  Feiges <71941165+ethanfeiges@users.noreply.github.com>
2022-01-11 21:58:14 -0800

    Add files via upload

--- algorithms.js
+++ algorithms.js
@@ -1,74 +1,62 @@
-var user = {
-	name: 'Bob',
-    	age: 20,
-	height: 72,
-	weight: 200,
-	weightGoal: 180,
-	sex: 'M',
-	currExercise: 2
+var name = form.name.value;
+var age = form.age.value;
+var height = form.height.value * 2.54; // in to centimeters
+var weight = form.weight.value * .454; // lb to kg
+var sex = form.gender.value;
+var BMR = 0; // ideal calorie intake
+var idealWeight = 0;
+if(sex == "Male"){
+  BMR = 13.397*weight + 4.799*height - 5.677*age + 88.362;
+  idealWeight = 48.0*weight + 2.7(form.height.value - 60); // GJ Hamwi (uses inches)
+}
+else{ // female
+  BMR = 9.247*weight + 3.098*height - 4.330*age + 447.593;
+  idealWeight = 45.5*weight + 2.2(form.height.value - 60);
+}
+var BMI = weight / ((100*height) * (100 * height));
+
+
+var METfactor = 0;
+var weeks = 4 + Math.round(Math.min(0, weight - idealWeight)/10); // weeks to exercise
+var totalCalorieLoss = .1 * BMR; // if not overweight (burn 10% of calroie intake)
+
+
+if(BMI >  25){
+  totalCalorieLoss = .2 * BMR; // ideally want to lose more weight
+  METfactor = 1; // increase intensity of each exercise by a factor of one.  
+}
+if(age > 65){ // longer duration, less intense exercises
+  weeks += 1;
+  METfactor = -1;
 }
 
-function BMI_Calc() {
-	hMeters = user.height/39.3701 // converting height in inches to meters
-	wKilo = user.weight/2.205  // converting weight in pounds to kilos
-	return wKilo/(hMeters*hMeters)  // calculating BMI
-}
-
-function idealWeight() {
-	return 22 * (user.height*0.0254) *(user.height*0.0254) * 2.20462 //weight(lb) = ideal BMI(22) * height(in meters) * height(in meters) * 2.20462 (kilos to pounds)
-}
-
-function WorkoutPlan() {
-    var status = ''
-    BMI = BMI_Calc()
-    // assigns at status depending on BMI
-    if (BMI <= 18.5) {
-        status = 'U'
-    }
-    else if (BMI > 18.5 && BMI <= 24.9) {
-        status = 'N'
-    }
-    else if (BMI > 24.9 && BMI < 30) {
-        status = 'Ov'
-    }
-    else {
-        status = 'Ob'
-    }
-    
-    // creates workout plan, considering status and age
-    if (user.age > 15 && user.age < 45) {
-        if (status == 'U') {
-            return 'Workout Type: Strength Training\t\tWeekly Hours: 3'
-        }
-        else if (status == 'N') {
-            return 'Workout Type: Strength Training\t\tWeekly Hours: 1\nWorkout Type: Aerobic Exercise\t\tWeekly Hours: 2'
-        }
-        else if (status == 'Ov') {
-            return 'Workout Type: Strength Training\t\tWeekly Hours: 3\nWorkout Type: Aerobic Exercise\t\tWeekly Hours: 3'
-        }
-        if (status == 'Ob') {
-            return 'Workout Type: Strength Training\t\tWeekly Hours: 1\nWorkout Type: Aerobic Exercise\t\tWeekly Hours: 4'
-        }
-    }
-    else if (user.age >= 45) {
-        if (status == 'U') {
-            return 'Workout Type: Aerobic Exercise\t\tWeekly Hours: 3'
-        }
-        else if (status == 'N') {
-            return 'Workout Type: Aerobic Exercise\t\tWeekly Hours: 3'
-        }
-        else if (status == 'Ov') {
-            return 'Workout Type: Aerobic Exercise\t\tWeekly Hours: 4'
-        }
-        if (status == 'Ob') {
-            return 'Workout Type: Aerobic Exercise\t\tWeekly Hours: 5'
-        }
-    }
-    else {
-        return 'Workout Type: Aerobic Exercise\t\tWeekly Hours: 3'
-    }
 
 
-}
+/* calories burned per minute * minutes = total calorie loss. 
+minutes = total calorie loss / calories burned per minute
+*/
+// Calories burned per minute: (Weight(kg) / 200) * MET * 3.5 
+Monday{
+  minutes: Math.round(.2(totalCalorieLoss) / ((weight / 200) * (3.3 + MET) * 3.5)),
+  description: "Brisk Walking"
+};
 
-console.log(WorkoutPlan())
+Tuesday{
+   minutes: Math.round(.2(totalCalorieLoss) / ((weight / 200) * (5 + MET) * 3.5)),
+   description: "Aerobatics"
+};
+
+Wednesday{
+  minutes: Math.round(.15(totalCalorieLoss) / ((weight / 200) * (7 + MET) * 3.5)),
+  description: "Core exerises"
+};
+
+Thursday{
+  minutes: Math.round(.3(totalCalorieLoss) / ((weight / 200) * (7 + MET) * 3.5)),
+  description: "Jogging"
+};
+
+Friday{
+  minutes: Math.round(.15(totalCalorieLoss) / ((weight / 200) * (4.5 + MET) * 3.5)),
+  description: "Power yoga"
+};
